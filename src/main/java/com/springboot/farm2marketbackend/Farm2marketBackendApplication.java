6190bda1cc756768279c7d24734bdb05d7b6a904
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-07-23 12:32:57 +0900

    Initial commit

---
+++ src/main/java/com/springboot/farm2marketbackend/Farm2marketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.farm2marketbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farm2marketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Farm2marketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/farm2marketbackend/Farm2marketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.farm2marketbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Farm2marketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
