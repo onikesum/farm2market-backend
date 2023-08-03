946395aba3c373225de84eb977951bae411970db
milkioiy <93613157+milkioiy@users.noreply.github.com>
2023-08-03 22:40:19 +0900

    Implement login, signup
    Implement login and signup functionality using Spring Security

---
+++ src/main/java/com/springboot/farm2marketbackend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.farm2marketbackend.repository;
+
+import com.springboot.farm2marketbackend.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User getByUid(String uid);
+
+}
